d0dd4c0da4c5fe16d14509d36f8b8bf3a2e24c84
sofvanh <23138848+sofvanh@users.noreply.github.com>
2022-02-01 15:06:36 +0200

    Add dynamic output

--- src/component/Editor.tsx
+++ src/component/Editor.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
-export default class Editor extends React.Component {
-    render () {
+export interface Props {
+    // TODO probably add something here
+}
+
+interface State {
+    text: string,
+}
+
+export default class Editor extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this); // This feels cheaty - from reactjs.org example
+        this.state = { text: 'Add your text!' };
+    }
+
+    handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        this.setState({ text: e.target.value })
+    }
+
+    render() {
         return (
             <div>
-                <textarea/>
+                <textarea
+                    id="markdown-content"
+                    onChange={this.handleChange}
+                    defaultValue={this.state.text}
+                />
+                <h3>Output here:</h3>
+                {this.state.text}
             </div>
         )
     }
