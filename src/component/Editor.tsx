8052b6ee33d82555d13a6b8482fccbd81634d098
sofvanh <23138848+sofvanh@users.noreply.github.com>
2022-02-01 15:19:06 +0200

    Render Markdown

--- src/component/Editor.tsx
+++ src/component/Editor.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import * as Remarkable from 'remarkable';
 
 export interface Props {
     // TODO probably add something here
 }
 
 interface State {
-    text: string,
+    text: string
 }
 
 export default class Editor extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
+        var something = 1;
+        this.md = new Remarkable.Remarkable();
         this.handleChange = this.handleChange.bind(this); // This feels cheaty - from reactjs.org example
         this.state = { text: 'Add your text!' };
     }
 
+    md; // I'm fairly sure this is not the right way to handle properties here
+
     handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
         this.setState({ text: e.target.value })
+    }
+
+    getRawMarkup() {
+        return { __html: this.md.render(this.state.text) };
     }
 
     render() {
@@ -28,7 +37,9 @@ export default class Editor extends React.Component<Props, State> {
                     defaultValue={this.state.text}
                 />
                 <h3>Output here:</h3>
-                {this.state.text}
+                <div
+                    dangerouslySetInnerHTML={this.getRawMarkup()} 
+                />
             </div>
         )
     }
