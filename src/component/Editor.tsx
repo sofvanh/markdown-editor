6e5a9430f2a2664b0c566d8fabe01c4143564e0d
sofvanh <23138848+sofvanh@users.noreply.github.com>
2022-02-01 15:33:34 +0200

    Move Preview into its own component

--- src/component/Editor.tsx
+++ src/component/Editor.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as Remarkable from 'remarkable';
+import Preview from './Preview';
 
 export interface Props {
     // TODO probably add something here
@@ -12,20 +12,12 @@ interface State {
 export default class Editor extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
-        var something = 1;
-        this.md = new Remarkable.Remarkable();
         this.handleChange = this.handleChange.bind(this); // This feels cheaty - from reactjs.org example
         this.state = { text: 'Add your text!' };
     }
 
-    md; // I'm fairly sure this is not the right way to handle properties here
-
     handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
         this.setState({ text: e.target.value })
-    }
-
-    getRawMarkup() {
-        return { __html: this.md.render(this.state.text) };
     }
 
     render() {
@@ -37,9 +29,7 @@ export default class Editor extends React.Component<Props, State> {
                     defaultValue={this.state.text}
                 />
                 <h3>Output here:</h3>
-                <div
-                    dangerouslySetInnerHTML={this.getRawMarkup()} 
-                />
+                <Preview text={this.state.text}/>
             </div>
         )
     }
---
+++ src/component/Preview.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as Remarkable from 'remarkable';
+
+export interface Props {
+    text: string
+}
+
+function Preview({ text }: Props) {
+    return (
+        <div
+            className="preview"
+            dangerouslySetInnerHTML={getRawMarkup(text)}
+        />
+    );
+}
+
+export default Preview;
+
+function getRawMarkup(text: string) {
+    var md = new Remarkable.Remarkable(); // TODO do this better
+    return { __html: md.render(text) };
+}
